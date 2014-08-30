7f4c0d2a4a4a1efe6e16396c71e9143f3955bed3
RichNusGeeks <richnusgeeks@gmail.com>
2014-08-30 16:25:25 -0700

    Update genericroutines.sh
    Addition of global macros and color output option.

--- genericroutines.sh
+++ genericroutines.sh
@@ -2,21 +2,27 @@
 ############################################################################
 # File name : genericroutines.sh
 # Purpose : A general purpose routines library for shell utilities.
-# Usages  : 
+# Usages  : Source in your bash utilities using . <Path>/genericroutines.sh
 # Start date : 21/04/2014
 # End date   : dd/mm/2014
-# Author : None
-# Download link : http://www.Ankur-Satellite-L755.com
+# Author : Ankur Kumar <richnusgeeks@gmail.com>
+# Download link : https://github.com/richnusgeeks/smmlnd
 # License : GNU GPL v3 http://www.gnu.org/licenses/gpl.html
 # Version : 0.0.1
 # Modification history : 
 ############################################################################
 
+#set -u
+ECHO=$(which echo)
+SED=$(which sed)
+GREP=$(which grep)
+
 # Prints error/warning/info message and exit/resume.
 # Arguments:
 #   message
 #   message type
-#   resume
+#   resume [=exit]
+#   color [=no]
 # Returns:
 #   None
 prntErrWarnInfo() {
@@ -24,6 +30,7 @@ prntErrWarnInfo() {
   local msg="$1"
   local msgtype="$2"
   local extrsme="$3"
+  local color="$4"
 
   if [ -z "$msg" ]; then
     printf "\n %s\n" "ERROR: No message provided"
@@ -48,7 +55,22 @@ prntErrWarnInfo() {
       ;;
   esac
 
-  printf "\n %s\n" "$msg"
+  if [ ! -z "$color" ]
+  then
+    if [ "$color" != yes ] && [ "$color" != no ]
+    then
+      printf "\n %s\n" "ERROR: Color option should be yes/no"
+      return
+    fi
+
+    case "$msgtype" in
+      err) msg="\033[31;40;1m$msg\033[m" ;;
+      info) msg="\033[32;40;1m$msg\033[m" ;;
+      warn) msg="\033[33;40;1m$msg\033[m" ;;
+    esac
+  fi
+
+  "$ECHO" -e "\n $msg\n"
   if [ "$extrsme" = "exit" ]; then
     printf " %s\n" "exiting ..."
     exit 1
@@ -71,31 +93,30 @@ getValFrmCnfg() {
 
   if [ $# -ne 3 ]
   then
-    prntErrWarnInfo "Not proper number of arguments passed" 
+    prntErrWarnInfo "Not proper number of arguments passed" err exit yes
   fi
 
   if [ -z "$section" ]
   then
-    prntErrWarnInfo "No section name provided"
+    prntErrWarnInfo "No section name provided" err exit yes
   fi
 
   if [ -z "$field" ]
   then
-    prntErrWarnInfo "No field name provided"
+    prntErrWarnInfo "No field name provided" err exit yes
   fi
   
   if [ -z "$cnfgfl" ]
   then
-    prntErrWarnInfo "No config file name provided"
+    prntErrWarnInfo "No config file name provided" err exit yes
   fi
 
   if [ ! -f "$cnfgfl" ]
   then
-    prntErrWarnInfo "No file $cnfgfl found"
+    prntErrWarnInfo "No file $cnfgfl found" err exit yes
   fi
 
-  local val=$(sed -n "/^ *$section/,/$ *^/p" $cnfgfl | grep "^ *$field" | sed "s/^ *$field *= *//" | sed 's/ \{1,\}$//')
+  local val=$("$SED" -n "/^ *$section/,/$ *^/p" $cnfgfl | "$GREP" "^ *$field" | "$SED" "s/^ *$field *= *//" | "$SED" 's/ \{1,\}$//')
   printf "%s" "$val"
 
 }
-
